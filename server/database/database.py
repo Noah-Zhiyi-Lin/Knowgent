f85eeb8ba49f817f9d0d109a86209a035e9abd51
Noah Zhiyi Lin <noah@Origin.local>
2024-11-10 21:33:50 +0800

    finish class Database

--- server/database/database.py
+++ server/database/database.py
@@ -4,20 +4,96 @@ import sqlite3
 class Database:
     def __init__(self):
         # Path of the database file (attention that the name of database file is fixed)
-        self.db_path = Path(__file__).parent / "database.db"
-        # Check whether the database file exists
-        db_exists = self.db_path.is_file()
-        # Connect to the database (database file will be created if it does not exist)
-        self.connection = sqlite3.connect(self.db_path)
-        # Get the cursor of the database
-        self.cursor = self.connection.cursor()
-        # If the database file was newly created, initialize it
-        if not db_exists:
-            self._initialize_database()
+        self.__db_path = Path(__file__).parent / "database.db"
+        # Connection to the database
+        self.__connection = None
+        # The cursor of the database
+        self.__cursor = None
 
-    def _initialize_database(self):
+    def connect(self):
+        """
+        Connect to the database
+        """
+        # Database file will be created if it does not exist
+        self.__connection = sqlite3.connect(self.__db_path)
+        self.__cursor = self.__connection.cursor()
+
+    def close(self):
+        """
+        Close the database
+        """
+        if not self.__connection:
+            raise ValueError("Database connection is not initialized")
+        self.__connection.close()
+
+    def commit(self):
+        """
+        Commit changes to the database
+        """
+        if not self.__connection:
+            raise ValueError("Database connection is not initialized")
+        self.__connection.commit()
+
+    def execute(self, sql, params=None):
+        """
+        Execute a SQL statement on the database
+        :param sql: sql statement to be executed
+        :param params: parameters to be passed into the sql statement
+        :return: None
+        """
+        if not self.__cursor:
+            raise ValueError("Cursor is not initialized. Please check the database connection")
+        if params is None:
+            params = []
+        # Execute the SQL statement
+        self.__cursor.execute(sql, params)
+        # Commit changes
+        self.commit()
+
+    def fetchone(self, sql, params=None):
+        """
+        Fetch one row from the query result
+        :param sql: sql statement to be executed
+        :param params: parameters to be passed into the sql statement
+        :return: the first row of the result, if nothing matches the sql statement, return None
+        """
+        if not self.__cursor:
+            raise ValueError("Cursor is not initialized. Please check the database connection")
+        if params is None:
+            params = []
+        self.__cursor.execute(sql, params)
+        return self.__cursor.fetchone()
+
+    def fetchall(self, sql, params=None):
+        """
+        Fetch all rows from the query result
+        :param sql: sql statement to be executed
+        :param params: parameters to be passed into the sql statement
+        :return: a list of tuples and one row is one tuple
+        """
+        if not self.__cursor:
+            raise ValueError("Cursor is not initialized. Please check the database connection")
+        if params is None:
+            params = []
+        self.__cursor.execute(sql, params)
+        return self.__cursor.fetchall()
+
+    def initialize(self):
+        """
+        Initialize the database
+        :return: None
+        """
+        self.connect()
+        self.create_tables()
+        self.crete_indices()
+
+    def create_tables(self):
+        """
+        Create the tables in the database
+        :return: None
+        """
         # Create table of notebooks
-        self.cursor.execute('''
+        create_notebooks_table = """
             CREATE TABLE IF NOT EXISTS notebooks (
                 id INTEGER PRIMARY KEY,
                 notebook_name TEXT NOT NULL UNIQUE,
@@ -25,9 +101,9 @@ class Database:
                 created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
-        ''')
+        """
         # Create table of notes, notebook_id is a foreign key
-        self.cursor.execute('''
+        create_notes_table = """
             CREATE TABLE IF NOT EXISTS notes (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL UNIQUE,
@@ -37,16 +113,16 @@ class Database:
                 modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (notebook_id) REFERENCES notebooks (id) ON DELETE CASCADE
             )
-        ''')
+        """
         # Create table of tags
-        self.cursor.execute('''
+        create_tags_table = """
             CREATE TABLE IF NOT EXISTS tags (
                 id INTEGER PRIMARY KEY,
                 tag_name TEXT NOT NULL UNIQUE
             ) 
-        ''')
-        # Create table of relation among notes and their tags
-        self.cursor.execute('''
+        """
+        # Create association table for notes and tags
+        create_note_tags_table = """
             CREATE TABLE IF NOT EXISTS note_tags (
                 note_id INTEGER,
                 tag_id INTEGER,
@@ -54,11 +130,72 @@ class Database:
                 FOREIGN KEY (note_id) REFERENCES notes (id) ON DELETE CASCADE,
                 FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
             )
-        ''')
-        # Create indices
-        self.cursor.execute("CREATE INDEX IF NOT EXISTS notebook_name_idx ON notebooks (notebook_name)")
-        self.cursor.execute("CREATE INDEX IF NOT EXISTS title_idx ON notes (title)")
-        self.cursor.execute("CREATE INDEX IF NOT EXISTS tag_name_idx ON tags (tag_name)")
-        # Commit transaction
-        self.connection.commit()
+        """
+        # Execute creation
+        self.execute(create_notebooks_table)
+        self.execute(create_notes_table)
+        self.execute(create_tags_table)
+        self.execute(create_note_tags_table)
 
+    def crete_indices(self):
+        """
+        Create indices of the tables in the database
+        :return: None
+        """
+        create_notebook_index = "CREATE INDEX IF NOT EXISTS notebook_name_idx ON notebooks (notebook_name)"
+        create_note_index = "CREATE INDEX IF NOT EXISTS title_idx ON notes (title)"
+        create_tag_index = "CREATE INDEX IF NOT EXISTS tag_name_idx ON tags (tag_name)"
+        # Execute creation of indices
+        self.execute(create_notebook_index)
+        self.execute(create_note_index)
+        self.execute(create_tag_index)
+
+    def update_record(self, table, data, conditions):
+        """
+        Update a record in the database
+        :param table: name of the table to be updated
+        :param data: a dictionary with the data to be updated
+        :param conditions: a dictionary with the conditions determine which record should be updated
+        :return: None
+        """
+        # Construct the SQL statement
+        set_clause = ", ".join([f"{key} = ?" for key in data.keys()])
+        where_clause = " AND ".join([f"{key} = ?" for key in conditions.keys()])
+        sql = f"UPDATE {table} SET {set_clause} WHERE {where_clause}"
+        params = list(data.values()) + list(conditions.values())
+        # Execute update
+        self.execute(sql, params)
+
+    def begin_transaction(self):
+        """
+        Begin a transaction
+        :return: None
+        """
+        if not self.__connection:
+            raise ValueError("Database connection is not initialized")
+        # Disable auto-commit
+        self.__connection.isolation_level = None
+        if not self.__cursor:
+            raise ValueError("Cursor is not initialized. Please check the database connection")
+        # Begin a transaction
+        self.__cursor.execute("BEGIN;")
+
+    def commit_transaction(self):
+        """
+        Commit a transaction
+        :return: None
+        """
+        if not self.__connection:
+            raise ValueError("Database connection is not initialized")
+        # Enable auto-commit
+        self.__connection.isolation_level = ''
+        self.__connection.commit()
+
+    def rollback_transaction(self):
+        """
+        Rollback a transaction
+        :return: None
+        """
+        if not self.__connection:
+            raise ValueError("Database connection is not initialized")
+        self.__connection.rollback()
